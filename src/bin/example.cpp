7308ef7258522835785449ed9bd495903b5f68e2
Roberto Metere <roberto@metere.it>
2021-03-16 23:06:07 +0000

    print out parsed ini file

--- src/bin/example.cpp
+++ src/bin/example.cpp
@@ -4,7 +4,16 @@
 
 int main(void) {
   CIniReader reader("example.ini");
-  reader.print();
+    
+  for (CIniSection *section: reader.sections()) {
+    if (section->name() != "") {
+      std::cout << std::endl << "[" << section->name() << "]" << std::endl;
+    }
+    for (std::pair<std::string, std::string> p: section->fields()) {
+      std::cout << p.first << " = " << p.second << std::endl;
+    }
+  }
+  
   return 0;
 }
 //-----------------------------------------------------------------------------
